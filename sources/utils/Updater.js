2d0bd3d6d0f786fbc4adb4148230b33a531e0bc9
HmHmmHm <hmkuak@naver.com>
2016-07-29 17:42:04 +0000

    Updater now can show lastest commit detail

--- sources/utils/Updater.js
+++ sources/utils/Updater.js
@@ -2,19 +2,19 @@
 
 module.exports = {
     /* global minejs */
-    onEnable: ()=>{
+    onEnable: () => {
         var updaterInstance = null;
-        minejs.utils.Updater = class Updater{
-            constructor(){
-                if(minejs.Server.getServer().getCluster().isWorker)
+        minejs.utils.Updater = class Updater {
+            constructor() {
+                if (minejs.Server.getServer().getCluster().isWorker)
                     return null;
-                if(updaterInstance != null)
+                if (updaterInstance != null)
                     return updaterInstance;
-                    
+
                 let lang = minejs.Server.getServer().getLang();
                 let logger = minejs.Server.getServer().getLogger();
-                
-                this.updater = new (require('auto-updater'))({
+
+                this.updater = new(require('auto-updater'))({
                     pathToJson: '',
                     autoupdate: false,
                     checkgit: false,
@@ -30,6 +30,14 @@ module.exports = {
                     logger.info(String(lang.updater_now_outdated_version));
                     logger.info(String(lang.updater_now_version_and_new_version)
                         .replace('%old_version%', v_old).replace('%version%', v));
+
+                    minejs.utils.Updater.getInstance().getGitHubCommits((body) => {
+                        logger.info(lang.updater_commit_name
+                            .replace('%message%', body[0].commit.message)
+                            .replace('%name%', body[0].commit.committer.name));
+                        logger.info(lang.updater_commit_upload_time
+                            .replace('%date%', body[0].commit.committer.date));
+                    });
                     logger.info(lang.updater_you_can_use_update_command);
                 });
                 this.updater.on('update.downloaded', function() {
@@ -55,17 +63,44 @@ module.exports = {
                     logger.info(lang.updater_error_in_update);
                     logger.info(name + ':' + err);
                 });
-                
                 this.updater.fire('check');
                 updaterInstance = this;
             }
             
-            downloadUpdate(){
+            /**
+             * @description
+             * @param {function} func
+             */
+            getGitHubCommits(func) {
+                let options = {
+                    hostname: 'api.github.com',
+                    port: 443,
+                    path: '/repos/organization/minejs/commits',
+                    method: 'GET',
+                    headers: {
+                        'user-agent': 'MineJS'
+                    }
+                };
+
+                let body = '';
+                let request = require('https').request(options, (response) => {
+                    response.on('data', (chunk) => {
+                        body += chunk;
+                    });
+                    response.on("end", function() {
+                        body = JSON.parse(body);
+                        func(body);
+                    });
+                });
+                request.end();
+            }
+
+            downloadUpdate() {
                 this.updater.fire('download-update');
             }
-            
-            static getInstance(){
-                if(!updaterInstance)
+
+            static getInstance() {
+                if (!updaterInstance)
                     new this();
                 return updaterInstance;
             }
