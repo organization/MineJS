2c373268fe37e76461041b8d1f7d63451745dc48
JellyBrick <shlee1503@naver.com>
2017-08-02 09:26:12 +0000

    Bug fix ['send' is undefined]

--- sources/Server.js
+++ sources/Server.js
@@ -202,7 +202,7 @@ class Server {
             workers[count++] = this.getCluster().workers[key];
 
         let target = workers[index];
-        if (target === null) {
+        if (target == null) {
             for (let key in this.getCluster().workers) {
                 target = this.getCluster().workers[key];
                 break;
